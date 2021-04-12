e1c31b3392d2120dfe295b51ce28e4c4c4550978
Alexander Volz <Alexander@volzit.de>
2021-04-13 00:10:43 +0200

    debug addeddevwarning

--- packaging/Docs/adddevwarning.sh
+++ packaging/Docs/adddevwarning.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-for file in $(find . -name *.md -type f); do echo -e "\n$file: ";  sed -i '1i\\' $file && sed -i '1i\\' $file && sed -i '1i>This is a dev version of our homepage! Please use [makroboard.app](https://makroboard.app) insted.\' $file && sed -i '1i>[!WARNING]\' $file ; done
+echo "list docs"
+find ./Docs 
+echo "execute"
+for file in $(find ./Docs -name *.md -type f); do echo -e "\n$file: ";  sed -i '1i\\' $file && sed -i '1i\\' $file && sed -i '1i>This is a dev version of our homepage! Please use [makroboard.app](https://makroboard.app) insted.\' $file && sed -i '1i>[!WARNING]\' $file ; done
