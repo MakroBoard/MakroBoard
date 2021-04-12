4413bdf36a395b62e61108e540ae84023109a253
Alexander Volz <Alexander@volzit.de>
2021-04-13 00:04:21 +0200

    add devwarning script

---
+++ packaging/Docs/adddevwarning.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+for file in $(find . -name *.md -type f); do echo -e "\n$file: ";  sed -i '1i\\' $file && sed -i '1i\\' $file && sed -i '1i>This is a dev version of our homepage! Please use [makroboard.app](https://makroboard.app) insted.\' $file && sed -i '1i>[!WARNING]\' $file ; done
