12180edef63491b9c5504da40fdf667c9f649bfd
Alexander Volz <Alexander@volzit.de>
2021-04-13 00:13:30 +0200

    debug devwarningscript

--- packaging/Docs/adddevwarning.sh
+++ packaging/Docs/adddevwarning.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "list docs"
-find ./Docs 
+find ./Docs -name *.md -type f
+echo "list docs without type"
+find ./Docs -name *.md
 echo "execute"
 for file in $(find ./Docs -name *.md -type f); do echo -e "\n$file: ";  sed -i '1i\\' $file && sed -i '1i\\' $file && sed -i '1i>This is a dev version of our homepage! Please use [makroboard.app](https://makroboard.app) insted.\' $file && sed -i '1i>[!WARNING]\' $file ; done
