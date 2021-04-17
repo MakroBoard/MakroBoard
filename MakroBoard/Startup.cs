d6b53f9e8edfe104f59fe96eee8ebbd47463622c
Alexander Volz <Alexander@volzit.de>
2021-04-18 00:27:19 +0200

    added HSTS

--- MakroBoard/Startup.cs
+++ MakroBoard/Startup.cs
@@ -70,7 +70,7 @@ namespace MakroBoard
                 app.UseExceptionHandler("/Error");
 
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
+                app.UseHsts();
             }
 
             app.UseHttpsRedirection();
