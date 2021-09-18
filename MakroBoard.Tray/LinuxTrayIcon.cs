52c6943667c3a59b52b540262a093b630d0919a0
Alexander Volz <Alexander@volzit.de>
2021-09-19 00:03:46 +0200

    implemented basic linux tray

--- MakroBoard.Tray/LinuxTrayIcon.cs
+++ MakroBoard.Tray/LinuxTrayIcon.cs
@@ -1,14 +1,49 @@
-﻿#if WINDOWS
-#endif
-using System;
+﻿using System;
+using Gtk;
+using Gdk;
 
 namespace MakroBoard.Tray
 {
     internal class LinuxTrayIcon : ITrayIcon
     {
+
+        private static StatusIcon trayIcon;
+
         public void Show()
         {
-            throw new NotImplementedException();
+            Application.Init();
+
+            var app = new Application("app.makroboard.client.tray", GLib.ApplicationFlags.None);
+            app.Register(GLib.Cancellable.Current);
+
+            trayIcon = new StatusIcon(new Pixbuf ("/home/volza/projects/MakroBoard/MakroBoard/Design/Logo_small_256_256.png"));
+            trayIcon.Visible = true;
+            trayIcon.PopupMenu += OnTrayIconPopup;
+
+            Application.Run();
+
+        }
+        
+        static void OnTrayIconPopup (object o, EventArgs args) {
+        Menu popupMenu = new Menu();
+
+        ImageMenuItem menuItemOpenMakroBoard = new ImageMenuItem ("Open MakroBoard");
+        ImageMenuItem menuItemQuit = new ImageMenuItem ("Quit");
+        Gtk.Image quitimg = new Gtk.Image(Stock.Quit, IconSize.Menu);
+        Gtk.Image appimg = new Gtk.Image(Stock.Quit, IconSize.Menu);
+        
+        
+        menuItemOpenMakroBoard.Image = appimg;
+        popupMenu.Add(menuItemOpenMakroBoard);
+
+        menuItemQuit.Image = quitimg;
+        popupMenu.Add(menuItemQuit);
+
+        menuItemQuit.Activated += delegate { Application.Quit(); }; //todo application exit call
+        menuItemOpenMakroBoard.Activated += delegate { }; //todo open makroboard
+
+        popupMenu.ShowAll();
+        popupMenu.Popup();
         }
     }
 }
